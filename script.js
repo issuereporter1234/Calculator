71238b6d2afe1f34a84b23f395cfa09f978f3329
issuereporter1234 <118681835+issuereporter1234@users.noreply.github.com>
2025-06-08 22:00:01 +0200

    Calculator logic declare x, y, i and operator variables add functions for each operation add a function for calling a wanted operation functions use forEach loop to add event listeners to every button use if statements to diferentiate first number, second number, operator, and Clear button display x and y variables in corect order increment i apropriateley after = is clicked convert x and y variables to numbers

--- script.js
+++ script.js
@@ -1,9 +1,76 @@
-function operate(){
-    
-     return operation
+let x = 0
+let y = 0
+let operator
+let i = 0
+function add(x, y){
+    return x + y
 }
-const calculate = document.getElementById('calculate')
-const result = document.getElementById('result')
-calculate.addEventListener('click', () => {
-    result.textContent = operate()
+function subtract (x, y){
+    return x - y
+}
+function multiply(x, y){
+    return x * y
+}
+function divide (x, y){
+    return x / y
+}
+
+function operate(x, operator, y){
+    switch (operator){
+        case '+': return add (x, y)
+        case '-': return subtract(x, y)
+        case '*': return multiply(x,y)
+        case ':': return divide (x, y)
+        default : return 'ERROR'
+    }
+}
+
+const display = document.getElementById('display')
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        buttonText = button.innerText
+        if(i === 0 && !isNaN(buttonText) && (operator === undefined || operator === null)){
+            display.innerText = buttonText
+            x = buttonText
+            ++i
+        }
+        else if(!isNaN(buttonText) && (operator === undefined || operator === null)){
+            display.innerText += buttonText
+            x += buttonText 
+            
+        }
+        else if(buttonText === '+' || buttonText === '-' || buttonText === '*' || buttonText === ':'){
+            operator = buttonText
+
+
+        
+        }
+        else if (i !== 0 && !isNaN(buttonText) && (operator !== undefined || operator !== null)){
+            display.innerText = buttonText
+            y = buttonText
+            i = 0
+        }
+        else if(!isNaN(buttonText) && (operator !== undefined || operator !== null)){
+            display.innerText += buttonText
+            y += buttonText
+        }
+        
+        else if (buttonText === '='){
+
+            display.innerText = operate(Number(x), operator, Number(y))
+        }
+        else if (buttonText === 'Clear'){
+            i = 0
+            x = 0
+            y = 0
+            operator = null
+            display.innerText = 0
+
+        }
+    
+    })
 })
+
+
+
+
